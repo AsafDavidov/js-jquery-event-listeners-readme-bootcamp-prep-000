14e999cb630b837e907d7300401d4d18e9480421
Asaf Davidov <adavidov@conncoll.edu>
2018-05-10 22:10:19 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,12 +4,5 @@
 $(document).ready(function(){
 // call functions here
 
-$('h1').on("eventname", function(){
-    //console.log("clicked");
-});
-
-$('img').on('load', function(){
-  //actions you want to happen
-});
 
 });
